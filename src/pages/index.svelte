7d4b56785dad687f6a5f29e22a421467ead0e01e
Darryl Yeo <dev@darryl-yeo.com>
2021-02-10 16:57:08 -0800

    npx @roxi/routify init

---
+++ src/pages/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@roxi/routify'
+    metatags.title = 'My Routify app'
+    metatags.description = 'Description coming soon...'
+</script>
+
+<RoutifyIntro />
