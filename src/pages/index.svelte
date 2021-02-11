f838b9c8c8038dc2863778203e4ffa8b5eecd538
Darryl Yeo <dev@darryl-yeo.com>
2021-02-10 17:03:17 -0800

    Use tabs

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,5 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
+	import { metatags } from '@roxi/routify'
+	metatags.title = 'My Routify app'
+	metatags.description = 'Description coming soon...'
 </script>
-
-<RoutifyIntro />
--- src/sw.js
+++ src/sw.js
@@ -18,15 +18,15 @@ const fallbackImage = '404.svg'
 const files = self.__WB_MANIFEST // files matching globDirectory and globPattern in rollup.config.js
 
 const externalAssetsConfig = () => ({
-  cacheName: 'external',
-  plugins: [
-    RoutifyPlugin({
-      validFor: 60 // cache is considered fresh for n seconds.
-    }),
-    new ExpirationPlugin({
-      maxEntries: 50, // last used entries will be purged when we hit this limit
-      purgeOnQuotaError: true // purge external assets on quota error
-    })]
+	cacheName: 'external',
+	plugins: [
+		RoutifyPlugin({
+			validFor: 60 // cache is considered fresh for n seconds.
+		}),
+		new ExpirationPlugin({
+			maxEntries: 50, // last used entries will be purged when we hit this limit
+			purgeOnQuotaError: true // purge external assets on quota error
+		})]
 })
 
 
@@ -51,7 +51,7 @@ precacheAndRoute(files)
 skipWaiting() // auto update service workers across all tabs when new release is available
 clientsClaim() // take control of client without having to wait for refresh
 
-/** 
+/**
  * manually upgrade service worker by sending a SKIP_WAITING message.
  * (remember to disable skipWaiting() above)
  */
@@ -77,14 +77,14 @@ setDefaultHandler(new NetworkFirst(externalAssetsConfig()));
 
 // serve a fallback for 404s if possible or respond with an error
 setCatchHandler(async ({ event }) => {
-  switch (event.request.destination) {
-    case 'document':
-      return await matchPrecache(entrypointUrl)
-    case 'image':
-      return await matchPrecache(fallbackImage)
-    default:
-      return Response.error();
-  }
+	switch (event.request.destination) {
+		case 'document':
+			return await matchPrecache(entrypointUrl)
+		case 'image':
+			return await matchPrecache(fallbackImage)
+		default:
+			return Response.error();
+	}
 })
 
 
@@ -99,10 +99,10 @@ function hasFreshCache(event) { return !!freshCacheData(event) }
 
 /** Example condition */
 function hasWitheringCache(event) {
-  const cache = freshCacheData(event)
-  if (cache) {
-    const { cachedAt, validFor, validLeft, validUntil } = cache
-    // return true if half the fresh time has passed
-    return validFor / 2 > validFor - validLeft
-  }
+	const cache = freshCacheData(event)
+	if (cache) {
+		const { cachedAt, validFor, validLeft, validUntil } = cache
+		// return true if half the fresh time has passed
+		return validFor / 2 > validFor - validLeft
+	}
 }
