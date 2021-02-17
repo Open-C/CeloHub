ad02585561f801218b65d349a62927f0dadb764b
Darryl Yeo <dev@darryl-yeo.com>
2021-02-17 20:54:29 +0000

    Support more browsers

--- rollup.config.js
+++ rollup.config.js
@@ -60,7 +60,7 @@ export default {
                     postcss: {
 						plugins: [
 							postcssImport(),
-							autoprefixer()
+							autoprefixer({ browsers: "last 4 version" }),
 						]
 					},
                     defaults: { style: 'postcss' }
