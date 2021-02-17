9e8e3d75ba79eaf1ab7f4740bf908a1c5c9d62ec
Darryl Yeo <dev@darryl-yeo.com>
2021-02-17 05:54:36 +0000

    Colorful tags!

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import type { Tag } from '../data/projects'
 
-	const tagColors = ['#99e7be', '#d5d0ff', '#d6e1ff', '#ffc6aa', '#ffd6f2', '#e5e5e5']
+	const tagColors = ['var(--celo-gold)', 'var(--celo-violet)', 'var(--celo-red)', 'var(--celo-cyan)', 'var(--celo-blue)']
 
 	export let tags: Tag[]
 
+	function simpleHash(str) {
+		let hash = 0
+		for(let i = 0; i < str.length; i++){
+			hash = ((hash << 5) - hash) + str.charCodeAt(i)
+			hash = hash & hash
+		}
+		return hash
+	}
+
 	function getTagColor(tag){
-		tagColors
-		// return await crypto.subtle.digest('SHA-1', new TextEncoder().encode(tag))
+		return tagColors[Math.abs(simpleHash(tag)) % tagColors.length]
 	}
 </script>
 
@@ -21,8 +29,9 @@
 	}
 	.tag {
 		background-color: var(--tag-color, var(--celo-gray));
+		background-image: linear-gradient(#ffffff99, #ffffff99);
 		border-radius: 0.5em;
-		padding: 0.35em 0.75em;
+		padding: 0.3em 0.75em;
 		text-align: center;
 		display: inline-block;
 		flex: 1 0 auto;
@@ -31,7 +40,6 @@
 
 <div class="tags">
 	{#each tags as tag}
-		<span class="tag">{tag}</span>
-		<!-- <span class="tag" style="--tag-color: {getTagColor(tag)}">{tag}</span> -->
+		<span class="tag" style="--tag-color: {getTagColor(tag)}">{tag}</span>
 	{/each}
 </div>
