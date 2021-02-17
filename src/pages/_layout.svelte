b27b6da29ae2394b2ab9e6ac15908b61150d54c8
Darryl Yeo <dev@darryl-yeo.com>
2021-02-17 19:28:34 +0000

    Heart icon

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -275,7 +275,7 @@
 		</main>
 
 		<footer>
-			<span>Made with <img alt="love"> by OpenC</span>
+			<span>Made with <img alt="love" src="/images/branding/heart.png"> by OpenC</span>
 			<a href="https://github.com" targt="_blank">GitHub</a>
 			<!-- <span style="overflow: hidden; text-overflow: ellipsis">Donate: 0x0000000000000000000000000000000000000000</span> -->
 		</footer>
