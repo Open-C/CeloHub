505fcc7fb33f106201a303257d923bab4305efed
Darryl Yeo <dev@darryl-yeo.com>
2021-05-29 13:42:52 -0700

    Add "Download Valora" page

---
+++ src/pages/valora.svelte
@@ -0,0 +1,17 @@
+<!-- routify:options index=false --> 
+
+<h3>Download Valora</h3>
+<article class="project card static">
+	<img src="/images/thumbnails/Valora.png" width="250" />
+	<a href="https://raw.githubusercontent.com/Open-C/CeloHub/main/assets/Valora1.14.2.apk">
+		<button><img src="/images/link-icons/Android-Logo-Round@3x.png" style="height: 1.6em; vertical-align: -0.45em" /> Download Android APK</button>
+	</a>
+</article>
+
+
+<style>
+	.project {
+		justify-self: start;
+		text-align: center;
+	}
+</style>
