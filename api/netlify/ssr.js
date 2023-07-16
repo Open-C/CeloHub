0530a07333e06ee909f22dcfba50ab09c5fcce01
Darryl Yeo <dev@darryl-yeo.com>
2023-07-16 14:28:06 -0700

    Netlify: avoid top-level await

--- api/netlify/ssr.js
+++ api/netlify/ssr.js
@@ -1,25 +1,26 @@
 
 import { resolve } from 'path'
-import { readFileSync, writeFileSync } from 'fs'
+import { readFileSync } from 'fs'
 import { build } from 'esbuild'
 
 const scriptPath = resolve(__dirname, '../../../dist/build/main.js')
 const templatePath = resolve(__dirname, '../../../dist/__app.html')
 const bundlePath = resolve(__dirname, '../build/bundle.js')
 
-await build({ entryPoints: [scriptPath], outfile: bundlePath, bundle: true })
-
-const bundle = {
-    date: new Date,
-    script: readFileSync(bundlePath, 'utf8'),
-    template: readFileSync(templatePath, 'utf8')
-}
-
-const { script, template } = bundle
+const bundlePromise = build({ entryPoints: [scriptPath], outfile: bundlePath, bundle: true }).then(() => {
+    const bundle = {
+        date: new Date,
+        script: readFileSync(bundlePath, 'utf8'),
+        template: readFileSync(templatePath, 'utf8')
+    }
+    
+    return bundle
+})
 
 import { tossr } from 'tossr'
 
 exports.handler = async (event, context) => {
+    const { script, template } = await bundlePromise
     const qs = Object.entries(event.queryStringParameters)
         .map(([key, value]) => `${key}=${value}`)
         .join('&');
